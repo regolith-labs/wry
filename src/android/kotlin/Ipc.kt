63b93f483bc3befed5cfd6ec930a5c89692bf201
Alexander Binaei <bigtimetapin@gmail.com>
2025-07-06 16:48:53 -0700

    sign-message

--- src/android/kotlin/Ipc.kt
+++ src/android/kotlin/Ipc.kt
@@ -27,6 +27,7 @@ class Ipc(val webViewClient: RustWebViewClient) {
         @JvmStatic external fun cacheVm()
         @JvmStatic external fun sendPublicKey(publicKey: String)
         @JvmStatic external fun sendSignedTransaction(signedTransaction: String)
+        @JvmStatic external fun sendSignedMessage(signedMessage: String)
     }
 
     private external fun ipc(url: String, message: String)
